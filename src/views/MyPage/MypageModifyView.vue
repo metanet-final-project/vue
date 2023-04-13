85d00d96d77ed856f85ed7522fee0d52b61432c4
DESKTOP-OUQGQ5I\soh40 <soh4084@naver.com>
2023-04-13 17:36:13 +0900

    commit

--- src/views/MyPage/MypageModifyView.vue
+++ src/views/MyPage/MypageModifyView.vue
@@ -25,19 +25,25 @@
 							<td style="background-color: #f3f4f6; width: 30%; padding: 15px">
 								아이디
 							</td>
-							<td style="width: 70%">
+
+							<td style="width: 70%; padding: 15px">{{ loginId }}</td>
+						</tr>
+						<tr>
+							<td style="background-color: #f3f4f6; padding: 15px">이름</td>
+							<td>
 								<input
 									type="text"
 									class="form-control form-control-sm"
 									id="mypage-modify-id"
-									value="qwer123"
-									style="border: 1px solid black; width: 300px; padding: 10px"
+									v-model="name"
+									style="
+										border: 1px solid black;
+										font-size: 16px;
+										width: 300px;
+										padding: 10px;
+									"
 								/>
 							</td>
-						</tr>
-						<tr>
-							<td style="background-color: #f3f4f6; padding: 15px">이름</td>
-							<td style="padding: 15px">신소희</td>
 						</tr>
 						<tr>
 							<td style="background-color: #f3f4f6; padding: 15px">전화번호</td>
@@ -46,8 +52,13 @@
 									type="text"
 									class="form-control form-control-sm"
 									id="mypage-modify-id"
-									value="01012345678"
-									style="border: 1px solid black; width: 300px; padding: 10px"
+									v-model="phone"
+									style="
+										border: 1px solid black;
+										font-size: 16px;
+										width: 300px;
+										padding: 10px;
+									"
 								/>
 							</td>
 						</tr>
@@ -58,19 +69,26 @@
 									type="email"
 									class="form-control form-control-sm"
 									id="mypage-modify-id"
-									value="aa@aa.com"
-									style="border: 1px solid black; width: 300px; padding: 10px"
+									v-model="email"
+									style="
+										border: 1px solid black;
+										font-size: 16px;
+										width: 300px;
+										padding: 10px;
+									"
 								/>
 							</td>
 						</tr>
 						<tr>
 							<td style="background-color: #f3f4f6; padding: 15px">생년월일</td>
-							<td style="padding: 15px">2000.01.07</td>
+							<td style="padding: 15px">{{ birthOnly }}</td>
 						</tr>
 					</tbody>
 				</table>
 				<div class="btn-area">
-					<button type="button" class="btn-print2">변경</button>
+					<button type="button" class="btn-print2" @click="UpdateMember">
+						변경
+					</button>
 				</div>
 			</div>
 		</div>
@@ -81,6 +99,52 @@
 <script setup>
 import Navbar from '@/layouts/Navbar.vue';
 import Footer from '@/layouts/Footer.vue';
+import axios from 'axios';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+// const loginRequest = ref({});
+const loginId = ref('');
+const name = ref('');
+const email = ref('');
+const birth = ref('');
+const phone = ref('');
+const birthObject = ref('');
+const birthOnly = ref('');
+
+const getMemberInfo = async () => {
+	const res = await axios.get(
+		`/api/member/findByLoginId/${localStorage.getItem('loginId')}`,
+	);
+	DataTransfer.value = res.data;
+	console.log(res.data);
+	console.log(res.data.birth);
+	loginId.value = res.data.loginId;
+	name.value = res.data.name;
+	email.value = res.data.email;
+	birth.value = res.data.birth;
+	birthObject.value = new Date(birth.value);
+	birthOnly.value = birthObject.value.toISOString().slice(0, 10);
+	console.log(birthOnly);
+	phone.value = res.data.phone;
+};
+getMemberInfo();
+
+const UpdateMember = async () => {
+	const res = await axios.put(
+		`/api/member/update`,
+		localStorage.getItem('loginId'),
+	);
+	try {
+		if (res != null) {
+			alert('회원정보가 변경되었습니다.');
+			router.push({ name: 'Mypage' });
+		}
+	} catch (error) {
+		alert('error');
+	}
+};
 </script>
 <style scoped>
 .table-container {
