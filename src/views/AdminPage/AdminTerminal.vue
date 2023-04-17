d536a21433b7263b98f208f55d8a44d8b1a197f4
youngoh kim <dhkfrkduddh5@naver.com>
2023-04-17 21:29:36 +0900

    commit

--- src/views/AdminPage/AdminTerminal.vue
+++ src/views/AdminPage/AdminTerminal.vue
@@ -9,10 +9,15 @@
 					style="background-color: #f0f2f5; border-radius: 10px"
 				>
 					<h4 class="m-3 p-2">터미널 관리</h4>
-					<MaterialButton class="m-3" variant="contained" color="dark"
+					<MaterialButton
+						class="m-3"
+						variant="contained"
+						color="dark"
+						@click="goManageTerminal(terminal.id)"
 						>터미널등록</MaterialButton
 					>
 				</div>
+
 				<div class="container mt-4">
 					<div class="row">
 						<div class="col-lg-12">
@@ -20,25 +25,26 @@
 								<thead>
 									<tr class="text-center">
 										<th class="col-1">번호</th>
-										<th class="col-2">아이디</th>
+										<th class="col-2">고유번호</th>
 										<th class="col-2">이름</th>
-										<th class="col-3">이메일</th>
-										<th class="col-3">전화번호</th>
-										<th class="col-1">관리</th>
+										<th class="col-5">위치</th>
+										<th class="col-2">관리</th>
 									</tr>
 								</thead>
 								<tbody class="table-group-divider text-center">
-									<tr v-for="(member, idx) in memberList" :key="member.id">
+									<tr
+										v-for="(terminal, idx) in terminalList"
+										:key="terminal.id"
+									>
 										<td>{{ idx + 1 }}</td>
-										<td>{{ member.loginId }}</td>
-										<td>{{ member.name }}</td>
-										<td>{{ member.email }}</td>
-										<td>{{ member.phone }}</td>
+										<td>{{ terminal.id }}</td>
+										<td>{{ terminal.name }}</td>
+										<td>{{ terminal.location }}</td>
 										<td>
 											<MaterialButton
 												color="light"
 												class="input-group-static btn-sm mb-0"
-												@click="goManageMember(member.id)"
+												@click="goManageTerminal(terminal.id)"
 												>관리</MaterialButton
 											>
 										</td>
@@ -62,7 +68,7 @@
 									"
 								>
 									<div class="modal-header">
-										<h5 class="modal-title">회원관리</h5>
+										<h5 class="modal-title">터미널관리</h5>
 										<MaterialBadge
 											color="dark"
 											rounded
@@ -75,22 +81,13 @@
 									</div>
 									<div class="modal-body">
 										<form role="form" class="text-start p-3">
-											<label>회원고유번호</label>
+											<label>터미널고유번호</label>
 											<div class="input-group input-group-outline mb-2">
 												<input
 													type="text"
 													class="form-control"
 													disabled
-													v-model="member.id"
-												/>
-											</div>
-											<label>아이디</label>
-											<div class="input-group input-group-outline mb-2">
-												<input
-													type="text"
-													class="form-control"
-													disabled
-													v-model="member.loginId"
+													v-model="terminal.id"
 												/>
 											</div>
 											<label>이름</label>
@@ -98,48 +95,33 @@
 												<input
 													type="text"
 													class="form-control"
-													v-model="member.name"
-												/>
-											</div>
-											<label>이메일</label>
-											<div class="input-group input-group-outline mb-2">
-												<input
-													type="text"
-													class="form-control"
-													v-model="member.email"
-												/>
-											</div>
-											<label>전화번호</label>
-											<div class="input-group input-group-outline mb-2">
-												<input
-													type="text"
-													class="form-control"
-													v-model="member.phone"
-												/>
-											</div>
-											<!-- <label>권한</label>
-											<div class="input-group input-group-outline">
-												<input
-													type="text"
-													class="form-control"
-													v-model="member.role"
 													disabled
+													v-model="terminal.name"
 												/>
-											</div> -->
+											</div>
+											<label>위치</label>
+											<div class="input-group input-group-outline mb-2">
+												<input
+													type="text"
+													class="form-control"
+													v-model="terminal.location"
+												/>
+											</div>
+
 											<div class="modal-footer justify-content-between">
 												<MaterialButton
 													variant="contained"
 													color="dark"
 													class="mb-0"
-													@click.prevent="updateMember"
+													@click.prevent="updateTerminal"
 													>수정하기</MaterialButton
 												>
 												<MaterialButton
 													variant="contained"
 													color="warning"
 													class="mb-0"
-													@click.prevent="deleteMember(member.id)"
-													>회원삭제</MaterialButton
+													@click.prevent="deleteTerminal(terminal.id)"
+													>터미널삭제</MaterialButton
 												>
 											</div>
 										</form>
@@ -150,14 +132,29 @@
 						<div class="row">
 							<div class="col-3 offset-md-9">
 								<div class="input-group input-group-outline my-3">
-									<label class="form-label">회원검색</label>
+									<label class="form-label">터미널 이름검색</label>
 									<input
 										type="text"
+										v-model="terminalName"
 										class="form-control form-control-md"
 										placeholder
-										isrequired="false"
 									/>
+									<button
+										class="btn btn-outline-secondary input-group-text"
+										type="button"
+										@click="searchTerminal"
+									>
+										검색
+									</button>
 								</div>
+								<button
+									class="btn btn-outline-secondary input-group-text"
+									type="button"
+									@click="searchTerminal"
+								>
+									검색
+								</button>
+								<div class="input-group-append"></div>
 							</div>
 						</div>
 						<section class="py-4">
@@ -202,54 +199,72 @@ import Swal from 'sweetalert2';
 onMounted(() => {
 	setMaterialInput();
 });
-
-const member = ref();
-const memberList = ref([]);
+const terminal = ref();
+const terminalName = ref();
+const terminalList = ref();
 const showModal = ref(false);
 
-const findAllMember = async () => {
+const findAllTerminal = async () => {
 	try {
-		const result = await axios.get('/api/member/findAllMember');
+		const result = await axios.get('/api/terminal/find-all');
 		if (result.data != null) {
-			memberList.value = result.data;
+			terminalList.value = result.data;
 		}
 	} catch (error) {
 		console.error(error);
 	}
 };
-findAllMember();
+findAllTerminal();
 
-const goManageMember = async memberId => {
+const goManageTerminal = async terminalId => {
 	try {
-		const result = await axios.get(`/api/member/findById/${memberId}`);
+		const result = await axios.get(`/api/terminal/findById/${terminalId}`);
 		if (result.data != null) {
-			member.value = result.data;
+			terminal.value = result.data;
+			showModal.value = true;
 		}
 	} catch (error) {
 		console.error(error);
 	}
-	showModal.value = true;
 };
 
-const updateMember = async () => {
+const updateTerminal = async () => {
 	try {
-		await axios.put(`/api/member/update`, member.value);
-		showToast('success', '회원수정을 완료했습니다.');
+		await axios.put(`/api/terminal/update`, terminal.value);
+		showToast('success', '터미널수정을 완료했습니다.');
 		showModal.value = false;
+		findAllTerminal();
 	} catch (error) {
 		console.error(error);
-		showToast('error', '회원수정에 실패했습니다.');
+		showToast('error', '터미널수정에 실패했습니다.');
 	}
 };
 
-const deleteMember = async memberId => {
+const deleteTerminal = async terminalId => {
 	try {
-		await axios.delete(`/api/member/delete/${memberId}`);
-		showToast('success', '회원삭제를 완료했습니다.');
+		await axios.delete(`/api/terminal/delete/${terminalId}`);
+		showToast('success', '터미널삭제를 완료했습니다.');
 		showModal.value = false;
+		findAllTerminal();
 	} catch (error) {
 		console.error(error);
-		showToast('error', '회원삭제에 실패했습니다.');
+		showToast('error', '터미널삭제에 실패했습니다.');
+	}
+};
+
+const searchTerminal = async () => {
+	try {
+		const result = await axios.get(
+			`/api/terminal/findByName/${terminalName.value}`,
+		);
+		if (result != null) {
+			terminalList.value = result.data;
+			console.log(terminalList.value);
+			showToast('success', '검색 완료');
+		}
+	} catch (error) {
+		console.log(error);
+		showToast('error', '터미널 고유번호를 입력하세요');
 	}
 };
 
